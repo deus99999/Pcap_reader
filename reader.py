eb31bf9c6a66cf623e6a098b2af7dd1e0d0c2de5
deus99999 <rudenkooleksei@gmail.com>
2023-09-13 09:55:57 +0300

    getting a result like {'010': {'SAC': 230, 'SIC': 238}, '015': {'SID': 230}, '040': {'TrkN': 59118}, '060': {'V': 59118, 'G': 59118, 'CH': 59118, 'spare': 59118, 'Mode3A': 1774}

--- reader.py
+++ reader.py
@@ -1,26 +1,37 @@
 import dpkt
-from read_xml import get_length
 from asterix4py import AsterixParser
 
-with open('cat_62_dump.pcap', 'rb') as pcap_file:
-    pcap = dpkt.pcap.Reader(pcap_file)
-    for ts, buf in pcap:
-        eth = dpkt.ethernet.Ethernet(buf)
-        if isinstance(eth.data, dpkt.ip.IP):
-            try:
-                data = eth.ip.udp.data
-                print("Data:", data)
-                # hexdata = ":".join("{:02x}".format(ord(c)) for c in str(data))
-                #print(f'Parsing packet: {hexdata}')
+
+def convert_hex_to_dec(hexdata):
+    output = ""
+    for i in range(0, len(hexdata), 2):
+        sub_str = hexdata[i] + hexdata[i + 1]
+        output += convert_hex_to_dec(sub_str)
+    output += chr(int(hex, 16))
+    return output
+
+def get_bytes_data_from_xml():
+    with open('cat_62_dump.pcap', 'rb') as pcap_file:
+        pcap = dpkt.pcap.Reader(pcap_file)
+        for ts, buf in pcap:
+            eth = dpkt.ethernet.Ethernet(buf)
+            if isinstance(eth.data, dpkt.ip.IP):
+                try:
+                    data = eth.ip.udp.data
+                    # hexdata = ":".join("{:02x}".format(ord(c)) for c in str(data))
+                    # print(f'Parsing packet: {hexdata}')
+                    print(data)
 
 
 
-                a = AsterixParser(data)
-                print(a.decoded)
-            except Exception as e:
-                print(e)
 
-        print("\n#####################\n")
+                    a = AsterixParser(data)
+                    print(a.decoded)
+                except Exception as e:
+                    print(e)
+            print("\n#####################\n")
+            return data
 
 
 
+print(get_bytes_data_from_xml())
